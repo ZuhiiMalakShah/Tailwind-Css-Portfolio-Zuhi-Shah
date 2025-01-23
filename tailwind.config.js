50fbef112ddc666b8578da2064f6e49d7cf7676b
Zuhii mz <165068338+ZuhiiMalakShah@users.noreply.github.com>
2025-01-23 11:39:56 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js}", // Update this path to match your project structure
-  ],
+  content: ["./src/**/*.{html,js}"], // Adjust the path to match your files
   theme: {
     extend: {
       colors: {
@@ -13,10 +11,16 @@ module.exports = {
         text2: '#ffffff',
         text3: 'rgb(204, 0, 0)',
         overlay: 'rgba(12, 12, 12, 0.63)',
+        lightBg: '#ffffff',
+        lightPrimary: '#ffffff',
+        lightSecondary: '#000',
+        lightText1: '#000',
+        lightOverlay: 'rgba(255, 255, 255, 0.7)',
       },
       fontFamily: {
         raleway: ['Raleway', 'sans-serif'],
         marcellus: ['Marcellus', 'serif'],
+        code: ['source-code-pro', 'Menlo', 'Monaco', 'Consolas', 'Courier New', 'monospace'],
       },
     },
   },
